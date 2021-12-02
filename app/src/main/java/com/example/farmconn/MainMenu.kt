b218b82d7ea32dacfcd5b61de0adb1216d0e6789
Achaj <janekk3@op.pl>
2021-12-02 22:55:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/farmconn/MainMenu.kt
@@ -0,0 +1,11 @@
+package com.example.farmconn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
