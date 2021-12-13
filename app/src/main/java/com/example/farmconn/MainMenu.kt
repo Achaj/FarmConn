d544fef0ace19029fadda37f66d34a4253dcfb77
Achaj <janekk3@op.pl>
2021-12-13 23:54:57 +0100

    frame add maszyny nie działą custom size device

--- app/src/main/java/com/example/farmconn/MainMenu.kt
+++ app/src/main/java/com/example/farmconn/MainMenu.kt
@@ -3,6 +3,7 @@ package com.example.farmconn
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 
 class MainMenu : AppCompatActivity() {
@@ -13,6 +14,11 @@ class MainMenu : AppCompatActivity() {
         val imgBtnLocalize = findViewById<ImageButton>(R.id.imageButtonLocalization)
         imgBtnLocalize.setOnClickListener{
             val intent= Intent(this,MapsActivity::class.java)
+            startActivity(intent)
+        }
+        val btnMaschineActivity = findViewById<Button>(R.id.btnMachineActivitiSelect)
+        btnMaschineActivity.setOnClickListener{
+            val intent= Intent(this,MachineryMainActivity::class.java)
             startActivity(intent)
         }
     }
